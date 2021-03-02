18ce00fffc2c5f17f4c61536348ca0540398dc97
Dixon-obabo <cheche.oloo@gmail.com>
2021-03-03 00:54:24 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable camelcase */
+const functions = require("firebase-functions");
+const express=require("express");
+const request=require("request");
+const app=express();
+const https=require("https");
+const moment= require("moment-timezone");
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.main = functions.https.onRequest(app);
+app.get("/", (req, res)=>{
+  res.status(200).json({
+    name: "Dickson",
+  });
+});
+
+app.get("/access_toke", (req, res)=>{
+  const consumer_key="hLOFxsToHsXKR4pzTSVoYoAop3J93B7X";
+  const consumer_secret="PjfnfGEnm7aVfGba";
+  const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+
+  const auth="Basic "+ new Buffer(consumer_key+":"+consumer_secret).toString("base64");
+
+
+  request({
+    url: url,
+    headers: {
+      "Authorization": auth,
+    }}, (error, response, body)=>{
+    if (error!==null) {
+      res.json(error);
+      console.log(error);
+    } else {
+      const fom=new Date().toLocaleString("en-US", {timeZone: "Africa/Nairobi"});
+      const nope=moment.tz("Africa/Nairobi").format("YYYYMMDDHHmmss");
+      const cool= new Date(fom);// .toISOString();
+      const all=String(cool.getFullYear())+String(cool.getMonth()+1)+String(cool.getDate())+String(cool.getHours())+String(cool.getMinutes())+String(cool.getSeconds());
+      const year =cool.getFullYear();
+      res.status(200).json({
+        "date": cool,
+        "year": year,
+        "all": nope,
+      });
+    }
+  });
+});
