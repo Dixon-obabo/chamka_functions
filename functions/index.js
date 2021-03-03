353821006b3a24caa5e31d8c16a06f72f6313a86
Dixon-obabo <cheche.oloo@gmail.com>
2021-03-03 13:49:10 +0300

    access token middleware created and works

--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,13 @@ const moment= require("moment-timezone");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.main = functions.https.onRequest(app);
-app.get("/", (req, res)=>{
+app.get("/", (_req, res)=>{
   res.status(200).json({
     name: "Dickson",
   });
 });
 
-app.get("/access_toke", (req, res)=>{
+app.get("/access_toke", (_req, res)=>{
   const consumer_key="hLOFxsToHsXKR4pzTSVoYoAop3J93B7X";
   const consumer_secret="PjfnfGEnm7aVfGba";
   const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
@@ -29,7 +29,7 @@ app.get("/access_toke", (req, res)=>{
     url: url,
     headers: {
       "Authorization": auth,
-    }}, (error, response, body)=>{
+    }}, (error, _response, _body)=>{
     if (error!==null) {
       res.json(error);
       console.log(error);
@@ -45,5 +45,34 @@ app.get("/access_toke", (req, res)=>{
         "all": nope,
       });
     }
+  });
+});
+
+
+// eslint-disable-next-line require-jsdoc
+function _access_token(req, res, next) {
+  const consumer_key="hLOFxsToHsXKR4pzTSVoYoAop3J93B7X";
+  const consumer_secret="PjfnfGEnm7aVfGba";
+  const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  const auth="Basic "+ new Buffer(consumer_key+":"+consumer_secret).toString("base64");
+
+  request({
+    url: url,
+    headers: {
+      "Authorization": auth,
+    }}, (error, response, body)=>{
+    if (error!==null) {
+      res.json(error);
+      console.log(error);
+    }
+    req.access_token=(JSON.parse(body)).access_token;
+    next();
+  });
+}
+
+
+app.get("/yourname", _access_token, (req, res)=>{
+  res.status(200).json({
+    message: "hello stranger",
   });
 });
