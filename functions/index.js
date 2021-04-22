7833202f407a66c6db41e43b23b8bbc7cb13c93e
Dixon-obabo <cheche.oloo@gmail.com>
2021-04-22 13:18:25 +0300

    moved to using axios

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,14 @@ const functions = require("firebase-functions");
 const express=require("express");
 const axios=require("axios").default;
 const request=require("request");
-const app=axios;
+const app=express();
 const moment= require("moment-timezone");
 const admin=require("firebase-admin");
 const cors=require("cors");
 admin.initializeApp();
 const db=admin.database();
 const ds=admin.firestore();
-// app.use(cors({origin: true}));
+app.use(cors({origin: true}));
 
 exports.main = functions.https.onRequest(app);
 app.get("/", (_req, res)=>{
@@ -57,47 +57,39 @@ exports.Deposit_attempt=functions.database.ref("/Att_Depo/{pushId}").onCreate((s
     console.log(response.data.access_token);
     console.log("The data get has worked");
 
-
-    request(
-        {
-          method: "POST",
-          url: endpoint,
-          headers: {
-            "Authorization": "Bearer "+ response.data.access_token,
-          },
-          json: {
-            "BusinessShortCode": "174379",
-            "Password": pword,
-            "Timestamp": nope,
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": snapshot.val().amount,
-            "PartyA": snapshot.val().phonenum,
-            "PartyB": "174379",
-            "PhoneNumber": PhoneNumber,
-            "CallBackURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/stk/lmstk",
-            "AccountReference": snapshot.val().phonenum,
-            "TransactionDesc": " look the web version works",
-          },
-        },
-        function(error, respone, body) {
-          if (error) {
-            console.log(error);
-          }
-
-          db.ref("saf_deposit_req").child(keyy).set(body);
-          console.log(body);
-        }
-    );
+    axios({
+      method: "POST",
+      url: endpoint,
+      headers: {
+        "Authorization": "Bearer "+ response.data.access_token,
+      },
+      data: {
+        BusinessShortCode: "174379",
+        Password: pword,
+        Timestamp: nope,
+        TransactionType: "CustomerPayBillOnline",
+        Amount: snapshot.val().amount,
+        PartyA: snapshot.val().phonenum,
+        PartyB: "174379",
+        PhoneNumber: PhoneNumber,
+        CallBackURL: "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/stk/lmstk",
+        AccountReference: snapshot.val().phonenum,
+        TransactionDesc: " look the web version works",
+      },
+    }).then((respon)=>{
+      console.log(respon.data);
+    }).catch((err)=>{
+      console.log(err);
+    });
   }).catch((error)=>{
     console.log(error);
   });
   return null;
 });
 
-exports.Loan_attempt= functions.database.ref("/Att_Loan/{pushId}").onCreate((snapshot, context )=>{
+exports.Loan_attempt=functions.database.ref("/Att_Loan/{pushId}").onCreate((snapshot, context )=>{
   // db.ref("/yourname").set("cool bruh it works");
   // console.log(snapshot.val());
-
   const consumer_key="hLOFxsToHsXKR4pzTSVoYoAop3J93B7X";
   const consumer_secret="PjfnfGEnm7aVfGba";
   const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
@@ -106,9 +98,6 @@ exports.Loan_attempt= functions.database.ref("/Att_Loan/{pushId}").onCreate((sna
   const nope=moment.tz("Africa/Nairobi").format("YYYYMMDDHHmmss");
   const pword=new Buffer.from("174379"+"bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"+nope).toString("base64");
   let token="";
-  // app.post();
-
-
   axios({method: "get",
     url: url,
     headers: {
@@ -116,31 +105,16 @@ exports.Loan_attempt= functions.database.ref("/Att_Loan/{pushId}").onCreate((sna
     },
   }).then((response)=>{
     token=response.data.access_token;
+
     axios({
       method: "POST",
       url: endpoint,
       headers: {
-        "Authorization": "Bearer "+response.data.access_token,
-      },
-      data: JSON.stringify( {
-        "InitiatorName": "apitest",
-        "SecurityCredential": "Se0HgfA8WqRQ9WtQfKj9YjOSINYsQlUmojYlA40iIUxFAP4h8RgnJiG4AJ9A8suugSgWde+XVFolFa/bYg/+oms7tIanX1sYnaoFpIPuM2QTWNkkquo2YPJ/lpT+HKMDTisFjLA98g+lc1OyANEHTyxqi7iK/gJDbeGOe7mbNqaNOR6vnsQublR5tALJWjENW9Rmi3drpLtZLrdalym2YrCSkzqzoVPyp4tWNk/LS/7I4pBwuE5HjDx8Wu6n8Fee5J8QMlWckqxiITLe/1ap6EOlvpjW9fMy8Ir6Z97jaIirlUBe/NU3L1tOevx/GsFt0dXlCY4gn3g65STT4AYTTQ==",
-        "CommandID": "BusinessPayment",
-        "Amount": "2300",
-        "PartyA": "600000",
-        "PartyB": "254796142444",
-        "Remarks": "You Won Nigga",
-        "QueueTimeOutURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2btimeout",
-        "ResultURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/validate",
-        "Occasion": "no occasion",
-      }),
-    }).then((rs)=>{
-      console.log(rs.data);
-      // response.data(rs.data);
-    }).catch((er)=>{
-      console.log(er);
-    });
+        "Authorization": "Bearer "+ response.data.access_token,
+      }, data: {
 
+      },
+    });
     // request({
     //   method: "POST",
     //   url: endpoint,
@@ -154,7 +128,7 @@ exports.Loan_attempt= functions.database.ref("/Att_Loan/{pushId}").onCreate((sna
     //     "PartyA": "600000",
     //     "PartyB": "254796142444",
     //     "Remarks": "You Won Nigga",
-    //     "QueueTimeOutURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/timeout",
+    //     "QueueTimeOutURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/stk/lmstk",
     //     "ResultURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/validate",
     //     "Occasion": "no occasion",
     //   }}, function(error, response, body) {
@@ -162,8 +136,6 @@ exports.Loan_attempt= functions.database.ref("/Att_Loan/{pushId}").onCreate((sna
     //     console.log(error);
     //   }
     //   console.log(body);
-    //   response.statusCode(200);
-    //   console.log(response.body);
     // });
   }).catch((error)=>{
     console.log(error);
@@ -181,7 +153,7 @@ app.get("/access_toke", (_req, res)=>{
   const consumer_secret="PjfnfGEnm7aVfGba";
   const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
-  const auth="Basic "+ new Buffer(consumer_key+":"+consumer_secret).toString("base64");
+  const auth="Basic "+ new Buffer.allocUnsafe(consumer_key+":"+consumer_secret).toString("base64");
 
 
   request({
@@ -232,26 +204,29 @@ function _access_token(req, res, next) {
 
 app.get("/register", _access_token, (req, res)=>{
   const endpoint="https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
-
-  request({
-    method: "POST",
-    url: endpoint,
-    headers: {
-      "Authorization": "Bearer " + req.access_token,
-    }, json: {
-      "ShortCode": "600000",
-      "ResponseType": "Completed",
-      "ConfirmationURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/confirm",
-      "ValidationURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/validate",
-    },
-  }, (error, reponse, body)=>{
-    if (error) {
-      console.log(error);
-      res.json(error);
-    }
-    res.status(200).json(body);
-    console.log(body);
-  });
+  try {
+    request({
+      method: "POST",
+      url: endpoint,
+      headers: {
+        "Authorization": "Bearer " + req.access_token,
+      }, json: {
+        "ShortCode": "600000",
+        "ResponseType": "Completed",
+        "ConfirmationURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/confirm",
+        "ValidationURL": "https://us-central1-blogzone-master-aa630.cloudfunctions.net/main/c2b/validate",
+      },
+    }, (error, reponse, body)=>{
+      if (error) {
+        console.log(error);
+        res.json(error);
+      }
+      res.status(200).json(body);
+      console.log(body);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/c2b/confirm", (req, res)=>{
@@ -264,45 +239,19 @@ app.post("/c2b/confirm", (req, res)=>{
 
   db.ref("/comp").set(res.body);
 });
-app.post("/c2b/timeout", (req, res, next)=>{
-  console.log("........Timeout........");
+
+
+app.post("/c2b/validate", (req, res, next)=>{
+  console.log(".......validate........");
   console.log(req.body);
+  res.status(200);
   // next();
 });
 
-app.post("/c2b/validate", (req, res)=>{
-  console.log(".......validate........");
-  // console.log(req.body);
-  db.ref("Att_Loan").limitToLast(1).on("value", (snapshot, context)=>{
-    console.log("hello dickson ");
-    // const cool =JSON.stringify(snapshot.val());
-    // const key=JSON.parse(cool.split(":", 1).toString().substring(1));
-    // const all=cool.split(",");
-    // const time=all[4].toString().substring(12);
-    // const amo=all[0].split(":");
-    // const amount= JSON.parse(amo[2]);
-    // const uid= JSON.parse(all[5].toString().replace("}}", "").substring(9));
-    // const tim=JSON.parse(time);
-    // const trans={"type": "Loan",
-    //   "amount": amount,
-    //   "status": "Failed",
-    //   "userid": uid,
-    //   "timestamp": tim,
 
-    // };
-    // // console.log(amount);
-    // ds.collection("Transactions").add(trans);
-  });
-  res.status(200);
-}).catch((err)=>{
-  console.log(err);
-});
-
-
-app.get("/c2b/validate", (req, res, next)=>{
+app.get("/c2b/validate", (req, res)=>{
   console.log(req.body);
-  // res.status(200);
-  next();
+  res.status(200);
 });
 
 app.get("/stk", _access_token, (req, res)=>{
@@ -337,7 +286,7 @@ app.get("/stk", _access_token, (req, res)=>{
         },
       },
       function(error, response, body) {
-        if (error) {
+        if (error!=null) {
           console.log(error);
         }
         res.json(body);
@@ -414,4 +363,3 @@ app.post("/stk/lmstk", (req, res)=>{
 
   res.status(200);
 });
-
